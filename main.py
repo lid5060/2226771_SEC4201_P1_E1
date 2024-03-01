6ae79a3e6611505aa7406851a53e084d0b987b0f
kyle <kp3crt@bolton.ac.uk>
2024-03-01 12:01:42 +0000

    Initial commit refrences added

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# bibliography
+#Ghadge, D., 2023. Building a Command-Line Menu in Python: Exploring Different Applications. [Online]
+#Available at: https://medium.com/@dghadge2002/building-a-command-line-menu-in-python-exploring-different-applications-f970a61e6412
+#[Accessed 1 March 2024]
+#w3schools, n.d. Python Try Except. [Online]
+#Available at: https://www.w3schools.com/python/python_try_except.asp
+#[Accessed 1 March 2024]
